eb0bfaf3982b694ab805330f219fc56ebf9db2f1
Genesis <rubiragene17@gmail.com>
2021-03-04 18:02:13 -0500

    prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, DashboardComponent, NavbarComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
