564504f39ae2db19677a606374e1be3215f6e12d
Genesis <rubiragene17@gmail.com>
2021-03-05 15:44:16 -0500

    app completa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular003';
+  dadoIzquierdo:string='../assets/img/dado1.png';
+  dadoDerecho:string='../assets/img/dado2.png';
+numeroInferior:number=0;
+numeroSuperior:number=0;
+  lanzarDado():void{
+   this.numeroInferior = Math.round(Math.random()*6)+1;
+   this.numeroSuperior=Math.round(Math.random()*6)+1;
+   if(this.numeroInferior>6){
+   this.numeroInferior=this.numeroInferior-1;
+   }
+   if(this.numeroSuperior>6){
+    this.numeroSuperior=this.numeroSuperior-1;
+    }
+     this.dadoIzquierdo='../assets/img/dado'+this.numeroInferior+'.png';
+     this.dadoDerecho='../assets/img/dado'+this.numeroSuperior+'.png'
+  }
 }
